b4109a39f8f6327bdcf2a51b82a83651c0d9b4d4
nara <naran.wot@gmail.com>
2023-05-02 16:33:41 +0800

    ok

--- src/components/QRmenu/QrmenuGrid.js
+++ src/components/QRmenu/QrmenuGrid.js
@@ -1,4 +1,4 @@
-import { Button, Row, Col, Alert, Space, message  } from "antd";
+import { Button, Row, Col, Alert, Space, message } from "antd";
 import React, { useContext, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { BellOutlined } from "@ant-design/icons";
@@ -11,12 +11,13 @@ const initialState = {
 
 const QrmenuGrid = () => {
   const [state, setState] = useState(initialState);
-  const [loading, setLoading] = useState(false)
+
+  const [loading, setLoading] = useState(false);
   const ctx = useContext(PosContext);
   const { id } = useParams();
-  
+
   const callWaiter = (id1) => {
-    console.log("called"); 
+    console.log("called");
     const messagecode = 190001;
     const configload = {
       headers: {
@@ -25,25 +26,20 @@ const QrmenuGrid = () => {
         request_code: messagecode,
       },
     };
-    const data = {      
-      token: "Bearer ADFLKJDFEREWFEEFSDFSDFDSFSDF"
+    const data = {
+      token: id,
     };
     setLoading(true);
     axios
       .post("/public/request", data, configload)
       .then((response) => {
-        console.log(response);
+        const data1 = response.data;
+        message.info("Амжилттай");
         setLoading(false);
-        if (response.data.code == 200) {            
-          message.info("Амжилттай");
-           
-          return;
-        }
-         
       })
-      .catch((error) => {       
+      .catch((error) => {
         message.warning("Амжилтгүй ");
-        console.log(error);       
+        console.log(error);
         setLoading(false);
       });
   };
@@ -55,48 +51,65 @@ const QrmenuGrid = () => {
     sessionStorage.clear();
   }, []);
   return (
-    <div style={{ height: "100vh", overflowX: "scroll",}}>
-      
-    <Row style={{ background: "transparent",   }}>
-      <Col span={24} align="center">
-      <div style={{background: "#F6FFED", border: "1px solid #c6f598", top: 0, position: "relative", padding: "10px", fontSize: "20px", fontWeight: "bold"}}>
-      ЦАХИМ МЕНЮ  -  ALPHA BAR & PUB 
+    <div style={{ height: "100vh", overflowX: "scroll" }}>
+      <Row style={{ background: "transparent" }}>
+        <Col span={24} align="center">
+          <div
+            style={{
+              background: "#F6FFED",
+              border: "1px solid #c6f598",
+              top: 0,
+              position: "relative",
+              padding: "10px",
+              fontSize: "20px",
+              fontWeight: "bold",
+            }}
+          >
+            ЦАХИМ МЕНЮ - ALPHA BAR & PUB
+          </div>
+
+          <div style={{ padding: "10px" }}>
+            <Button
+              type="primary"
+              loading={loading}
+              icon={<BellOutlined />}
+              onClick={(id) => callWaiter(id)}
+            >
+              Үйлчилгээний ажилтан дуудах
+            </Button>
+          </div>
+        </Col>
+
+        <div>
+          <Col span={24} align="left">
+            {ctx.state.baraaangilal?.map((el) => (
+              <>
+                <Alert message={el.name} type="info" />
+                <Row>
+                  {ctx.state.baraanuud
+                    ?.filter((el1) => el1.trl == el.id)
+                    ?.map((el9) => (
+                      <Space size={[8, 16]} wrap>
+                        <BaraaItem
+                          nme={el9.nme}
+                          zurag={
+                            ctx.state.baraanuudImage?.find(
+                              (elimage) => elimage.id == el9.id
+                            )?.pht
+                          }
+                          une={el9.une}
+                        />
+                      </Space>
+                    ))}
+                </Row>
+              </>
+            ))}
+          </Col>
+        </div>
+      </Row>
+      <div style={{ padding: "20px", textAlign: "center", height: "50px" }}>
+        Developed by ALPHA TEAM 2023
       </div>
-      
-      <div style={{padding: "10px"}}>
-        
-      <Button type="primary" loading={false} icon={<BellOutlined />} onClick={(id)=>callWaiter(id)}>
-          Үйлчилгээний ажилтан дуудах
-        </Button>
-    
-      </div>
-         
-      </Col>
-      
-      <div >
-      <Col span={24} align="left">
-        {ctx.state.baraaangilal?.map((el) => (
-          <>
-            <Alert message={el.name} type="info" /> 
-            <Row>
-            {ctx.state.baraanuud
-              ?.filter((el1) => el1.trl == el.id)
-              ?.map((el9) => (
-                <Space size={[8, 16]} wrap>
-              <BaraaItem nme={el9.nme} zurag={ctx.state.baraanuudImage?.find((elimage)=>elimage.id == el9.id)?.pht} une={el9.une} />
-                </Space>
-                
-              ))}            
-            </Row>
-            
-          </>
-        ))}
-      </Col>
-      </div>
-    </Row>
-    <div style={{ padding: "20px", textAlign: "center", height: "50px"}}>
-      Developed by ALPHA TEAM 2023
-    </div>
     </div>
   );
 };
